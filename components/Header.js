7e4094b46d4eceed4d1fb2d68e9d81fe8bd5b0c9
JasonRillera <jarillera@gmail.com>
2021-08-09 18:06:44 -0700

    update header

--- components/Header.js
+++ components/Header.js
@@ -29,12 +29,12 @@ function Header() {
 
 
             <div className="flex items-center space-x-4 justify-end text-gray-500">
-                <p>Become A Host</p>
-                <GlobeAltIcon className="h-6"/>
+                <p className="hidden md:inline-flex cursor-pointer">Become A Host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer"/>
 
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <MenuIcon className="h-6" />
-                    <UserCircleIcon className="h-6" />
+                    <MenuIcon className="h-6 cursor-pointer" />
+                    <UserCircleIcon className="h-6 cursor-pointer" />
                 </div>
             </div>
 

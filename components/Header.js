028865e8b0a520a505d986349453715443231724
JasonRillera <jarillera@gmail.com>
2021-08-09 17:17:16 -0700

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+
+function Header() {
+    return (
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-red-500">
+
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image
+                    src="http://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                />
+            </div>
+
+
+            <div></div>
+
+
+            <div></div>
+
+        </header>
+    )
+}
+
+export default Header

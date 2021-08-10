69a17a0ec6a490d51dd6bf4f9cdaed821811146a
JasonRillera <jarillera@gmail.com>
2021-08-10 15:25:37 -0700

    front end update

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 bg-gray-100 px-32 py-14 border-t text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+            </div>
+    
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>But it's awesome</p>
+            <p>Referrals accepted</p>
+            <p>Jason Rillera</p>
+            </div>
+    
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>Jason Rillera</p>
+            <p>Presents</p>
+            <p>A cool Airbnb Site</p>
+            <p>Made with</p>
+            <p>Tailwind CSS</p>
+            </div>
+    
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Say Hello</p>
+            <p>World</p>
+            <p>For the Win</p>
+            </div>
+        </div>
+        );
+    }
+
+export default Footer;

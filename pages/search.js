2c25b4d7a3226529d2320bbe2eaeed63c478bbb5
JasonRillera <jarillera@gmail.com>
2021-08-13 15:08:13 -0700

    update search

--- pages/search.js
+++ pages/search.js
@@ -8,17 +8,16 @@ function Search() {
 
             <main className="flex">
                 <section>
-                    <p className="text-xs ">300+ Stays for 6 Guests</p>
+                    <p className="text-xs">300+ Stays for 6 Guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in SF</h1>
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>
+                    </div>
                 </section>
-                
-                <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-                    <p className="button">Cancellation Flexibility</p>
-                    <p className="button">Type of place</p>
-                    <p className="button">Price</p>
-                    <p className="button">Rooms and Beds</p>
-                    <p className="button">More filters</p>
-                </div>
 
             </main>
 
